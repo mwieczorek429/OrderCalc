bed43eb091430c367cb228a7b6dc9a98225d8765
Mateusz Wieczorek <mateusz.wieczorek429@gmail.com>
2025-03-05 20:37:54 +0100

    Initial commit

---
+++ OrderCalc.Tests/ProductTests.cs
@@ -0,0 +1,29 @@
+﻿using OrderCalc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderCalc.Tests
+{
+    public class ProductTests
+    {
+        [Fact]
+        public void Construcot_ShouldCreateProduct_WithValidParameters() 
+        {
+            //Arrange
+            string name = "Apple";
+            decimal price = 10.5m;
+            int quantity = 3;
+
+            //Act
+            var product = new Product(name, price, quantity);
+
+            //Assert
+            Assert.Equal(name, product.Name);
+            Assert.Equal(price, product.Price);
+            Assert.Equal(quantity, product.Quantity);
+        }
+    }
+}
---
+++ OrderCalc/Models/Order.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderCalc.Models
+{
+    public class Order : IOrder
+    {
+        private List<IProduct> _products = new();
+
+        public void AddProduct(IProduct product)
+        {
+            _products.Add(product);
+        }
+
+        public decimal GetTotalPrice()
+        {
+            decimal total = 0;
+            foreach (IProduct product in _products)
+            {
+                total += product.Price;
+            }
+
+            return total;
+        }
+    }
+}
